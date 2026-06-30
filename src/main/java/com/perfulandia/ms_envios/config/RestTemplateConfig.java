c4afaf0f2251b77ece4804e1f94261b4bd786110
Antonella Castillo <ant.castillos@duocuc.cl>
2026-06-30 08:41:01 -0400

    rest template agregado

--- src/main/java/com/perfulandia/ms_envios/config/RestTemplateConfig.java
+++ src/main/java/com/perfulandia/ms_envios/config/RestTemplateConfig.java
@@ -1,5 +1,17 @@
 package com.perfulandia.ms_envios.config;
 
-public class RestTemplateConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
+// CAPA CONFIG: registra el RestTemplate como @Bean para comunicarse con otros MS.
+@Configuration
+public class RestTemplateConfig 
+{
+
+    @Bean
+    public RestTemplate restTemplate() 
+    {
+        return new RestTemplate();
+    }
 }
